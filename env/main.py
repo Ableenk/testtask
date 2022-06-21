78685a0935e354c41839905642590382f23f473b
ableenk <ableenk@protonmail.com>
2022-06-21 04:40:24 +0300

    now works perfectly!

--- env/main.py
+++ env/main.py
@@ -20,7 +20,7 @@ def get_data(directory_name: str, sample_size=+inf) -> tuple:
     bid_prices = np.stack(tmp['bid'], axis=0)
     return (ask_prices, bid_prices)
 
-# @njit
+@njit
 def get_weights(size, q):
     weights = np.zeros(0) 
     weight = 1
@@ -29,7 +29,7 @@ def get_weights(size, q):
         weight *= q
     return weights[::-1]
 
-# @njit
+@njit
 def get_ewma(data_array: np.ndarray, window_size, q: float):
     length = data_array.shape[0]
     weights = get_weights(window_size, q)
@@ -41,10 +41,12 @@ def get_ewma(data_array: np.ndarray, window_size, q: float):
         result = np.append(result, [new_ewma])
     return result
 
-# @njit
+@njit
 def generate_signal(ask_prices, bid_prices, long_threshold, short_threshold, short_window, long_window, q):
     current_prices = (ask_prices + bid_prices) / 2
     signals_array = np.zeros((current_prices.shape[0], current_prices.shape[1]))
+    if long_window < short_window:
+        long_window, short_window = short_window, long_window
     for active_id in range(current_prices.shape[0]):
         active = current_prices[active_id]
         short_ewmas = get_ewma(active, short_window, q)
@@ -53,6 +55,7 @@ def generate_signal(ask_prices, bid_prices, long_threshold, short_threshold, sho
         active_signals = np.zeros(max_window_length)
         for timestamp in range(max_window_length, active.shape[0]):
             error = long_ewmas[timestamp-long_window] - short_ewmas[timestamp-short_window]
+#             print(round(error, 3))
             if error > long_threshold:
                 new_signal = np.zeros(1) + 1
             elif error < short_threshold:
@@ -63,6 +66,7 @@ def generate_signal(ask_prices, bid_prices, long_threshold, short_threshold, sho
         signals_array[active_id] = active_signals
     return signals_array
 
+@njit
 def get_UPL(balance, currencies, ask_prices, timestamp):
     UPL = balance
     for active_id in range(ask_prices.shape[0]):
@@ -73,8 +77,7 @@ def run_backtest(ask_prices, bid_prices, signals_array, max_position_usd, commis
     balance = max_position_usd
     pq = PositionsQueue(delay)
     actives_count = ask_prices.shape[0]
-    currencies = [0] * actives_count
-    zero_UPL = get_UPL(balance, currencies, ask_prices, 0)
+    currencies = np.zeros(actives_count)
     UPLs = np.array([balance])
     for timestamp in range(min(sample_size, signals_array.shape[1])):
         position_amount = min(balance, max_position_usd) / actives_count
@@ -95,15 +98,15 @@ def run_backtest(ask_prices, bid_prices, signals_array, max_position_usd, commis
             if active_id in to_handle:
                 for handling in to_handle[active_id]:
                     amount = handling[0]
-                    balance -= commission
                     if handling[1]:
                         currencies[active_id] -= amount
-                        balance += amount * ask_prices[active_id][timestamp]
+                        balance_change = amount * ask_prices[active_id][timestamp]
                     else:
                         currencies[active_id] += amount
-                        balance -= amount * bid_prices[active_id][timestamp]
-            tmp_UPL = get_UPL(balance, currencies, ask_prices, timestamp)
-            UPLs = np.append(UPLs, (tmp_UPL))
+                        balance_change = -amount * bid_prices[active_id][timestamp]
+                    balance += balance_change - commission
+        tmp_UPL = get_UPL(float(balance), currencies, ask_prices, timestamp)
+        UPLs = np.append(UPLs, (tmp_UPL))
     return UPLs
 
 # short_window_size, long_window_size, long_threshold, short_threshold, q
@@ -116,23 +119,27 @@ def to_maximaze(x, ask_prices, bid_prices, max_position_usd, commission, delay):
 
 def get_arguments(ask_prices, bid_prices, max_position_usd, commission, delay):
     # short_window_size, long_window_size, long_threshold, short_threshold, q
-    bounds = ((8, 250), (8, 250), (-10000, 10000), (-10000, 10000), (0.4, 1.1)) 
+    # ДО
+    # bounds = ((8, 250), (8, 250), (-10000, 10000), (-10000, 10000), (0.4, 1.1))
+    # ПОСЛЕ
+    bounds = [(10, 200), (20, 200), (-0.1, 0.1), (-0.1, 0.1), (0.1, 0.9)] 
     result = scipy.optimize.differential_evolution(to_maximaze, bounds, args=(ask_prices, bid_prices, max_position_usd, commission, delay), disp=False, polish=False, updating='deferred')
     return result
 
+@njit
 def sharp_coef(UPLs):
     if UPLs.std() == 0:
         return 0
-    return UPLs.mean()/UPLs.std()                  
+    return ((UPLs-UPLs[0]).mean()/UPLs.std())                
 
 def main(directory_name: str):
-    max_position_usd = 100
-    commission = 1
-    delay = 10
+    max_position_usd = 500
+    commission = 2
+    delay = 15
     ask_prices, bid_prices = get_data(directory_name, sample_size = 500)
     result = get_arguments(ask_prices, bid_prices, max_position_usd, commission, delay)
-    long_threshold, short_threshold, window_size, last_weight = result.x
-    signals_array = generate_signal(ask_prices, bid_prices, long_threshold, short_threshold, window_size, last_weight)
+    short_window_size, long_window_size, long_threshold, short_threshold, q = result.x
+    signals_array = generate_signal(ask_prices, bid_prices, long_threshold, short_threshold, int(short_window_size), int(long_window_size), q)
     UPLs = run_backtest(ask_prices, bid_prices, signals_array, max_position_usd, commission, delay)
     print(-result.fun)
     lineplot_by_y(UPLs)
